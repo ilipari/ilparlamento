4fe8be2bb8d617dc9ad2017055eefc51e2f7e3c2
Ignazio Lipari <iglipari@gmail.com>
2026-06-04 02:14:44 +0200

    refactor

--- panelui/app.py
+++ panelui/app.py
@@ -4,19 +4,16 @@ from service.data import get_dataframe, data_index
 
 pn.extension()
 
-legislature = data_index()['camera']
-select = pn.widgets.Select(label='Select', options=legislature)
-data_pane = pn.pane.DataFrame()
-root = pn.Column(
-    select,
-    data_pane,
-)
-root.servable(title='Il parlamento')
-
 
 def update(legislature):
-    data_pane.object = get_dataframe('camera', legislature)
+    df = get_dataframe('camera', legislature)
+    return pn.pane.DataFrame(df)
 
 
-pn.bind(update, select, watch=True)
-
+legislature = data_index()['camera']
+select = pn.widgets.Select(label='Select', options=legislature)
+root = pn.Column(
+    select,
+    pn.bind(update, select),
+)
+root.servable(title='Il parlamento')
