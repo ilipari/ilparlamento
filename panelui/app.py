b5431240f1babaace455ba3f60863d1a7e7af8b7
Ignazio Lipari <iglipari@gmail.com>
2026-06-04 02:39:24 +0200

    select Institution name

--- panelui/app.py
+++ panelui/app.py
@@ -4,16 +4,27 @@ from service.data import get_dataframe, data_index
 
 pn.extension()
 
+name_to_legislature = data_index()
+select_institution = pn.widgets.Select(label='Institution', options=list(name_to_legislature.keys()))
+select_legislature = pn.widgets.Select(label='Legislatura', options=name_to_legislature[select_institution.value])
 
-def update(legislature):
-    df = get_dataframe('camera', legislature)
-    return pn.pane.DataFrame(df)
+
+def show_data(name, legislature):
+    if not legislature:
+        return
+    df = get_dataframe(name, legislature)
+    return pn.pane.DataFrame(df.head())
 
 
-legislature = data_index()['camera']
-select = pn.widgets.Select(label='Select', options=legislature)
+# Callback per l'aggiornamento delle legislature disponibili
+def update_select_legislature(name):
+    select_legislature.options = name_to_legislature[name]
+
+
+pn.bind(update_select_legislature, select_institution, watch=True)
 root = pn.Column(
-    select,
-    pn.bind(update, select),
+    select_institution,
+    select_legislature,
+    pn.bind(show_data, select_institution, select_legislature),
 )
 root.servable(title='Il parlamento')
